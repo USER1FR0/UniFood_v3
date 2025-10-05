9306152c36966e2ae5584f702d13bc024df65b98
USER1FR0 <f51783145@gmail.com>
2025-10-05 16:26:47 -0600

    Conexion a BD: 3000 (API), 4000(Notificaciones), 5000(Pagos)

--- unifood_micro_pagos/src/app.service.ts
+++ unifood_micro_pagos/src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap} from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnApplicationBootstrap{
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(private readonly dataSource: DataSource) {}
+
+  async onApplicationBootstrap() {
+      try {
+        await this.dataSource.query('SELECT 1');
+        this.logger.log('(Pagos),Conectado a la Base de Datos Exito');
+      }catch(error){
+        this.logger.error('Error al conectar a la Base de Datos', error);
+      }
+      this.logger.log('Servicio de Pagos Iniciado Puerto 5000');
+  }
+  
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! (Pagos)';
   }
 }
