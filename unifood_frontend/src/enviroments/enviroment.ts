7efe8d4b8f5422a999aa840bc1826f7108164735
USER1FR0 <f51783145@gmail.com>
2025-10-05 16:38:12 -0600

    configuracion completa

--- unifood_frontend/src/enviroments/enviroment.ts
+++ unifood_frontend/src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000/unifood/api',
-    pagosApiUrl: 'http://localhost:5000/pagos/api',
     notificacionesApiUrl: 'http://localhost:4000/notificaciones/api',
+    pagosApiUrl: 'http://localhost:5000/pagos/api',
 }
